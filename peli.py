68249c54c8c8d200f7240df175f50faf53a0b4ee
Pirjo Keskinen <pirjo.keskinen@tuni.fi>
2026-01-11 13:33:07 +0200

    update: peli.py Copyright

--- peli.py
+++ peli.py
@@ -1,3 +1,10 @@
+"""
+Operaatio Anthophila – Python Text Adventure
+© 2026 Pirjo Keskinen. Original work. All rights reserved.
+
+All story elements, locations, and characters are original creations.
+"""
+
 import time
 import sys
 import threading
